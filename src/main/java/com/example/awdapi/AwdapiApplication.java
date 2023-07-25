cba6c1d2adc744b493bdc9de7413ab29747a85ff
Ken Chan <chankamlam@icloud.com>
2023-07-25 10:35:05 +0800

    init

---
+++ src/main/java/com/example/awdapi/AwdapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.awdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwdapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwdapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/awdapi/AwdapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwdapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
